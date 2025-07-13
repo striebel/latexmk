9341ade6b34a935a4651e1a13e7ac98d2e4abd86
Jacob Striebel <striebel@users.noreply.github.com>
2025-07-12 23:20:36 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+latexmk
+-------
+
+project description
---
+++ src/latexmk/__main__.py
@@ -0,0 +1,18 @@
+import sys
+
+
+
+def main() -> int:
+
+    print('hello')
+
+    return 0
+
+
+
+if '__main__' == __name__:
+    sys.exit(main())
+
+
+
+
